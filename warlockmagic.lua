4350747f1eb59a8b3e759546fd56417bcbc85e0c
RaymondM945 <raymondm9820@gmail.com>
2025-07-22 20:21:46 -0400

    commit again

--- warlockmagic.lua
+++ warlockmagic.lua
@@ -1,4 +1,58 @@
 local isFollowing = nil
+local stopaddon = false
+local checkfollow = true
+local castshadowbolt = true
+
+local MyCheckbox = CreateFrame("CheckButton", "MyCheckboxExample", UIParent, "UICheckButtonTemplate")
+MyCheckbox:SetSize(30, 30)
+
+-- Anchor to the top center of the screen
+MyCheckbox:SetPoint("TOP", UIParent, "TOP", 0, -20) -- 20 pixels down from the top
+
+MyCheckbox.text = MyCheckbox:CreateFontString(nil, "ARTWORK", "GameFontNormal")
+MyCheckbox.text:SetPoint("LEFT", MyCheckbox, "RIGHT", 5, 0)
+MyCheckbox.text:SetText("stop addon")
+
+MyCheckbox:SetChecked(not stopaddon)
+
+MyCheckbox:SetScript("OnClick", function(self)
+	if self:GetChecked() then
+		print("Checkbox is checked!")
+		stopaddon = true
+	else
+		print("Checkbox is unchecked!")
+		stopaddon = false
+	end
+end)
+
+local MyCheckbox2 = CreateFrame("CheckButton", "MyCheckbox2", UIParent, "UICheckButtonTemplate")
+MyCheckbox2:SetSize(30, 30)
+MyCheckbox2:SetPoint("TOP", MyCheckbox, "BOTTOM", 0, 0) -- 10 pixels below the first checkbox
+
+MyCheckbox2.text = MyCheckbox2:CreateFontString(nil, "ARTWORK", "GameFontNormal")
+MyCheckbox2.text:SetPoint("LEFT", MyCheckbox2, "RIGHT", 5, 0)
+MyCheckbox2.text:SetText("Check following")
+
+MyCheckbox2:SetChecked(checkfollow)
+MyCheckbox2:SetScript("OnClick", function(self)
+	checkfollow = self:GetChecked()
+	print("Checkbox 2 is", self:GetChecked() and "checked" or "unchecked")
+end)
+
+local MyCheckbox3 = CreateFrame("CheckButton", "MyCheckbox3", UIParent, "UICheckButtonTemplate")
+MyCheckbox3:SetSize(30, 30)
+MyCheckbox3:SetPoint("TOP", MyCheckbox2, "BOTTOM", 0, 0) -- Directly under Checkbox2
+
+MyCheckbox3.text = MyCheckbox3:CreateFontString(nil, "ARTWORK", "GameFontNormal")
+MyCheckbox3.text:SetPoint("LEFT", MyCheckbox3, "RIGHT", 5, 0)
+MyCheckbox3.text:SetText("cast shadow bolt")
+
+MyCheckbox3:SetChecked(castshadowbolt)
+
+MyCheckbox3:SetScript("OnClick", function(self)
+	castshadowbolt = self:GetChecked()
+	print("Checkbox 3 is", isChecked and "checked" or "unchecked")
+end)
 
 local box = CreateFrame("Frame", "CombatRogueCenterBox", UIParent)
 box:SetSize(25, 25)
@@ -12,7 +66,7 @@ local f = CreateFrame("Frame")
 f:SetScript("OnUpdate", function(self, elapsed)
 	box.texture:SetColorTexture(0, 0, 0, 1)
 
-	if IsInGroup() then
+	if IsInGroup() and not stopaddon then
 		local targethealth = UnitHealth("party1target")
 		local targetmaxHealth = UnitHealthMax("party1target")
 		local hpPercent = (targethealth / targetmaxHealth) * 100
@@ -25,9 +79,13 @@ f:SetScript("OnUpdate", function(self, elapsed)
 			local sametarget = UnitIsUnit("target", "party1target")
 			local spell = UnitCastingInfo("player")
 
+			local sname = "Shadow Bolt"
+			local shadowisUsable, notEnoughMana = IsUsableSpell(sname)
+			local canCastShadowBolt = shadowisUsable and not notEnoughMana and spell ~= sname
+
 			if not sametarget then
 				box.texture:SetColorTexture(0, 0, 1, 1)
-			elseif not isFollowing then
+			elseif not isFollowing and checkfollow then
 				box.texture:SetColorTexture(1, 1, 1, 1)
 			elseif
 				not AuraUtil.FindAuraByName(corruptionName, "target", "HARMFUL") and IsUsableSpell(corruptionName)
@@ -41,6 +99,8 @@ f:SetScript("OnUpdate", function(self, elapsed)
 				box.texture:SetColorTexture(1, 0, 1, 1)
 			elseif not AuraUtil.FindAuraByName(curseOfReck, "target", "HARMFUL") and IsUsableSpell(curseOfReck) then
 				box.texture:SetColorTexture(0, 1, 1, 1)
+			elseif canCastShadowBolt and castshadowbolt then
+				box.texture:SetColorTexture(0.5, 1, 0.5, 1)
 			elseif not IsAutoRepeatSpell("Shoot") then
 				box.texture:SetColorTexture(0, 1, 0, 1)
 			end
